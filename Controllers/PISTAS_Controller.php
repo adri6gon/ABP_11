c791845f5474149d7f88c6054a5963e3ad7c4120
adri6gon <adri6gonza@gmail.com>
2018-11-06 21:20:06 +0100

    ACL,ReservarPista,ListarHora/Pista

---
+++ Controllers/PISTAS_Controller.php
@@ -0,0 +1,225 @@
+<?php
+if(!isset($_SESSION)){
+    session_start();
+}
+include_once '../Functions/Authentication.php';
+
+if (!IsAuthenticated()){
+	header('Location:../index.php');
+} 
+
+include_once '../Models/PISTAS_Model.php';
+include '../Views/PISTA_SHOWALL_View.php';
+include '../Views/RESERVA_SHOWALL_View.php';
+//include '../Views/PISTA_SEARCH_View.php';
+//include '../Views/PISTA_ADD_View.php';
+//include '../Views/PISTA_EDIT_View.php';
+//include '../Views/PISTA_DELETE_View.php';
+include '../Views/PISTA_SHOWCURRENT_View.php';
+include '../Views/MESSAGE_View.php';
+include '../Functions/ACL.php';
+
+function get_data_form(){
+	
+	 $idPista = $_REQUEST['idPista'];
+	 $restriccion= $_REQUEST['restriccion'];
+	 $nombre= $_REQUEST['nombre'];
+	 $hora= $_REQUEST['hora'];
+	 $fecha= $_REQUEST['fecha'];
+
+	$action = $_REQUEST['action'];
+	
+	$PISTAS = new PISTAS_Model(
+		 $idPista,
+		 $restriccion,
+		 $nombre,
+		 $hora,
+		 $fecha
+	);
+
+	return $PISTAS;
+}
+$lista = array('idPista','restriccion','nombre','hora','fecha');
+$funcionalidad = "PISTAS";
+$alerta = "No tiene permisos para esta acción.";
+
+//var_dump($_REQUEST['action']);
+//exit();
+if (!isset($_REQUEST['action'])){
+	$_REQUEST['action'] = '';
+}
+
+	Switch ($_REQUEST['action']){
+		case 'ADD':
+		//Comprobamos que tiene los permisos necesarios para realizar esta accion
+			if(comprobarRol('admin')){
+				if(!$_POST){//Si viene vacio
+                    //Nuevo modelo vacio
+					$PISTA = new PISTAS_Model('','','','','');
+                    //Nueva vista
+					$add = new ADD_PISTA('../Controllers/PISTAS_Controller.php');
+				}else{//Si no viene vacio
+                    //Recoge los datos con getdataform
+					$PISTA = get_data_form();
+                    //LLamas al add del modelo
+					$respuesta = $PISTA->ADD();				
+					include_once '../Functions/Authentication.php';
+					if(!IsAuthenticated()){//Si no esta autenticado
+						new MESSAGE($respuesta,'../Controllers/Login_Controller.php');
+					}else{//Si esta autenticado
+						new MESSAGE($respuesta,'../Controllers/PISTAS_Controller.php');
+					}
+				
+				}
+			}//Si no tiene los permisos mostramos el mensaje de alerta
+			else{
+				new MESSAGE($alerta,'../index.php');
+			}
+			break;
+		case 'EDIT':
+        //Si tiene permisos
+			if(comprobarRol('admin')){
+				if(!$_POST){//Si viene vacio
+                    //Nuevo modelo vaci0
+					$PISTA = new PISTAS_Model($_REQUEST['login'],'','','','');
+                    //Rellenas los datos con rellenadatos
+					$valores = $PISTA->RellenaDatos();
+					
+					//En el edit tenemos que poder asignar y desasignar grupos a un user por eso le pasamos 2 arrays:el de grupos que tiene asignados y el de todos los grupos
+					//La vista debe tener despues de los datos del PISTA una lista de checkbox para asignar/desasignar grupos
+					new MODIFY_USER($valores,'./PISTAS_Controller.php');
+				}
+				else{//Si no viene vacio
+                    //Recoges los datos con el dataform
+					$PISTA = get_data_form();
+					//var_dump($_REQUEST['grupos']);
+					//exit();
+                    //Haces el edit del modelo
+					$respuesta = $PISTA->EDIT();
+					
+					new MESSAGE($respuesta,'./PISTAS_Controller.php');
+					
+				}
+			}else{//Si no tiene permisos
+				new MESSAGE($alerta,'../index.php');
+			}
+			break;
+		case 'SHOWCURRENT':
+        //Si tiene permisos 
+			if(/*comprobarPermisos($_REQUEST['action'],$funcionalidad)*/comprobarRol($_REQUEST['action'])){
+                //nuevo modelo de PISTAS
+				$PISTAS = new PISTAS_Model($_REQUEST['idPista'], '', '', '','');
+                //Recoge los datos de PISTAS
+				$valores = $PISTAS->RellenaDatos();
+                //Nueva vista
+				new PISTA_SHOWCURRENT($valores,'../Controllers/PISTAS_Controller.php',$lista);
+			}else{//Si no tiene permisos
+				new MESSAGE($alerta,'../index.php');
+			}
+				break;
+		case 'PREVDAY':
+		//Si no teine permisos
+			if(comprobarRol('admin')){
+				if (!$_POST){//Si viene vacio
+                    //Sumamos un dia al recibido
+					$siguiente=date("Y-m-d",strtotime($_REQUEST['fecha'])-86400);
+					$PISTAS = new PISTAS_Model('','', '', '', $siguiente);
+				}
+				else{//Recoge los datos
+					$PISTAS = get_data_form();
+				}
+				//var_dump($PISTAS);
+				//exit();
+                //Llama al showall del modelo
+				$datos = $PISTAS->_SHOWALL();
+				//var_dump($PISTAS);
+				$pistas = $PISTAS->_SHOWNAMES();
+                //Nueva vista
+				new PISTA_SHOWALL($lista,$pistas, $datos, '../index.php');
+			}else{//Si no tiene permisos
+				new MESSAGE($alerta,'../index.php');
+			}
+			//Cambio de dia en el SHOWALL de PISTAS
+		break;
+		case 'NEXTDAY':
+		//Si no teine permisos
+			if(comprobarRol('admin')){
+				if (!$_POST){//Si viene vacio
+                    //Sumamos un dia al recibido
+					$siguiente=date("Y-m-d",strtotime($_REQUEST['fecha'])+86400);
+					$PISTAS = new PISTAS_Model('','', '', '', $siguiente);
+				}
+				else{//Recoge los datos
+					$PISTAS = get_data_form();
+				}
+				//var_dump($PISTAS);
+				//exit();
+                //Llama al showall del modelo
+				$datos = $PISTAS->_SHOWALL();
+				//var_dump($PISTAS);
+				$pistas = $PISTAS->_SHOWNAMES();
+                //Nueva vista
+				new PISTA_SHOWALL($lista,$pistas, $datos, '../index.php');
+			}else{//Si no tiene permisos
+				new MESSAGE($alerta,'../index.php');
+			}
+			//Cambio de dia en el SHOWALL de PISTAS
+		break;
+		case 'RESERVE':
+			if(comprobarRol('deportista')){
+                //nuevo modelo de PISTAS
+				$PISTAS = new PISTAS_Model($_REQUEST['idPista'], '', $_REQUEST['nombre'], '','');
+               	//Ejecutamos la reserva
+				$reserva = $PISTAS->RESERVE($_SESSION['login']);
+				new MESSAGE($reserva,'../Controllers/PISTAS_Controller.php');
+			}else{//Si no tiene permisos
+				new MESSAGE($alerta,'../Controllers/PISTAS_Controller.php');
+			}
+		break;
+		case 'RESERVAS':
+			if(comprobarRol('admin')){
+                //nuevo modelo de PISTAS
+				$PISTAS = new PISTAS_Model('', '', '', '','');
+               	//Ejecutamos la reserva
+				$lista = array('nombre','fecha','hora');
+				$reserva = $PISTAS->YOUR_RESERVES($_SESSION['login']);
+				new RESERVA_SHOWALL($reserva,$lista,'../Controllers/PISTAS_Controller.php');
+			}else{//Si no tiene permisos
+				new MESSAGE($alerta,'../Controllers/PISTAS_Controller.php');
+			}
+		break;
+		case 'DEL_RESERVA':
+			if(comprobarRol('admin')){
+                //nuevo modelo de PISTAS
+				$PISTAS = new PISTAS_Model($_REQUEST['idPista'], '', $_REQUEST['nombre'], '','');
+               	//Ejecutamos el borrado de la reserva
+				$reserva = $PISTAS->DEL_RESERVES($_SESSION['login']);
+				new MESSAGE($reserva,'../Controllers/PISTAS_Controller.php?action=RESERVAS');
+			}else{//Si no tiene permisos
+				new MESSAGE($alerta,'../Controllers/PISTAS_Controller.php');
+			}
+		break;
+		default: //Default entra el showall
+        //Si no teine permisos
+			if(comprobarRol('admin')){
+				if (!$_POST){//Si viene vacio
+                    //Nuevo modelo vacio
+					$PISTAS = new PISTAS_Model('','', '', '', '2018-10-10');
+				}
+				else{//Recoge los datos
+					$PISTAS = get_data_form();
+				}
+				//var_dump($PISTAS);
+				//exit();
+                //Llama al showall del modelo
+				$datos = $PISTAS->_SHOWALL();
+				//var_dump($PISTAS);
+				$pistas = $PISTAS->_SHOWNAMES();
+                //Nueva vista
+				new PISTA_SHOWALL($lista,$pistas, $datos, '../index.php');
+			}else{//Si no tiene permisos
+				new MESSAGE($alerta,'../index.php');
+			}
+	}
+
+?>
--- Views/users_menuLateral.php
+++ Views/users_menuLateral.php
@@ -1,8 +1,3 @@
-<!--
-Funcion del archivo: Vista que genera el menu lateral cuando un usuario esta logeado en la web
-Autor: j0z5zs
-Fecha: 23/11/17
--->
 	<!--Navegador PROVISIONAL-->
 		<div class="cuerpo">
 			<nav class="menu">
@@ -11,17 +6,21 @@ Fecha: 23/11/17
 				<ul>
 				<?php	
 					//Si tiene los permisos mostramos el enlace
-					if(comprobarRol('USUARIOS')){
+					if(comprobarRol('admin')){
 						echo "<li><a href='./USUARIOS_Controller.php'>USUARIOS</a></li>";
 					}
 					//Si tiene los permisos mostramos el enlace
-					if(comprobarRol('GRUPOS')){
+					if(comprobarRol('admin')){
 						echo '<li><a href="./GRUPOS_Controller.php">GRUPOS</a></li>';
 					}
 					//Si tiene los permisos mostramos el enlace
-					if(comprobarRol('TRABAJOS')){
-						echo '<li><a href="./TRABAJOS_Controller.php">TRABAJOS</a></li>';
-					}					
+					if(comprobarRol('admin')){
+						echo '<li><a href="./PISTAS_Controller.php">PISTAS</a></li>';
+					}	
+					//Si tiene los permisos mostramos el enlace
+					if(comprobarRol('admin')){
+						echo '<li><a href="./PISTAS_Controller.php?action=RESERVAS">RESERVAS</a></li>';
+					}	
 					
 				?>		
 				</ul>
