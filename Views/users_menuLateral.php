efc9c8dadfcb05bb188f31ca1cadeecae53355f6
noe <noe_ferreiro@hotmail.com>
2018-11-08 18:50:12 +0100

    PruebaNoe

---
+++ Views/users_menuLateral.php
@@ -0,0 +1,37 @@
+	<!--Navegador PROVISIONAL-->
+		<div class="cuerpo">
+			<nav class="menu">
+				<p>Menu</p>
+				
+				<ul>
+				<?php	
+					//Si tiene los permisos mostramos el enlace
+					if(comprobarRol('admin')){
+						echo "<li><a href='./USUARIOS_Controller.php'>USUARIOS</a></li>";
+					}
+					//Si tiene los permisos mostramos el enlace
+					if(comprobarRol('admin')){
+						echo '<li><a href="./GRUPOS_Controller.php">GRUPOS</a></li>';
+					}
+					//Si tiene los permisos mostramos el enlace
+					if(comprobarRol('admin')){
+						echo '<li><a href="./PISTAS_Controller.php">PISTAS</a></li>';
+					}	
+					//Si tiene los permisos mostramos el enlace
+					if(comprobarRol('admin')){
+						echo '<li><a href="./PISTAS_Controller.php?action=RESERVAS">RESERVAS</a></li>';
+					}	
+					//Si tiene los permisos mostramos el enlace
+					if(comprobarRol('admin')){
+						echo '<li><a href="./CAMPEONATOS_Controller.php">CAMPEONATOS</a></li>';
+					}	
+					//Si tiene los permisos mostramos el enlace
+					if(comprobarRol('admin')){
+						echo '<li><a href="./CATEGORIAS_Controller.php">CATEGORIAS</a></li>';
+					}	
+					
+				?>		
+				</ul>
+			</nav>
+			<section class="content">
+			
